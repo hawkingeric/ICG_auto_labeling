fc5098f17ca152eecf2b9d23118bb98e08562cf5
eric_pan <eric_pan@dynacolor.com.tw>
2022-06-10 15:31:01 +0800

    Refactor of functions

--- auto_labeling.py
+++ auto_labeling.py
@@ -8,6 +8,88 @@ from skimage .segmentation import active_contour
 from skimage import measure
 
 
+
+
+
+#---create one initial enclosing circle for avtive contour model(snakes)
+# snakes=[]
+# s = np.linspace(0, 2*np.pi, 400)
+# r = 100 + 105*np.sin(s)
+# c = 832 + 105*np.cos(s)
+# init = np.array([r, c]).T
+# snakes.append(active_contour(gaussian(img_thresh, 3, preserve_range=False), init, alpha=0.015, beta=10, gamma=0.001))
+
+
+
+
+
+def nothing(x):
+    pass
+
+def createGUI():
+	'''Functions that create the trackbar interface'''
+	cv2.createTrackbar(switch_color, title_window, 0, 1, nothing)
+	cv2.createTrackbar(switch_thresh, title_window, 0, 1, nothing)
+	cv2.createTrackbar(switch_relative, title_window, 0, 1, nothing)
+	cv2.createTrackbar(trackbar_thresh_abs, title_window, 90, thresh_abs_slider_max, nothing)
+	cv2.createTrackbar(trackbar_thresh_rel, title_window, 354, thresh_rel_slider_max, nothing)
+
+def updateSwitchValues(sc_last, st_last):
+	'''Functions that updates the Switch ON/OFF values'''
+	sc = cv2.getTrackbarPos(switch_color, title_window)
+	st = cv2.getTrackbarPos(switch_thresh, title_window)
+	if sc - sc_last > 0:                                    #---switch color ON
+		cv2.setTrackbarPos(switch_thresh, title_window, 0)  #---turn OFF thresh
+		st = 0                                              #---turn OFF thresh
+	if st - st_last > 0:                                    #---switch thresh ON
+		cv2.setTrackbarPos(switch_color, title_window, 0)   #---turn OFF color
+		sc = 0                                              #---turn OFF color
+	return sc, st
+
+def findGeoCenter():
+	for contour in contours:
+		M = cv2.moments(contour)  
+		centerX = int(M["m10"] / M["m00"])
+		centerY = int(M["m01"] / M["m00"])
+#		cv2.circle(img, (cX, cY), 5, (1, 227, 254), -1) #--- plot a filled circle at the center
+		return centerX, centerY
+
+def findSnakes():
+	snakes=[]
+	for contour in contours:
+		((x, y), radius) = cv2.minEnclosingCircle(contour)
+		scaling_factor=1.2
+		radius=radius*scaling_factor
+#		cv2.circle(img, (int(x), int(y)), int(radius), (0, 255, 0), 2) #--- plot minEnclosingCircle
+		angle = np.linspace(0, 2*np.pi, 400)
+		row = y + radius*np.sin(angle)
+		column = x + radius*np.cos(angle)
+		init = np.array([row, column]).T
+		snake = active_contour(img_thresh, init, alpha=0.015, beta=10, gamma=0.001)
+		snakes.append(snake)
+		return snakes
+
+
+def checkSwitchRelative():
+	sr = cv2.getTrackbarPos(switch_relative, title_window)
+	return sr
+
+def applyThresh(image_input):
+	if(checkSwitchRelative()):
+		thresh_rel = cv2.getTrackbarPos(trackbar_thresh_rel, title_window)
+		ret, img_thresh = cv2.threshold(image_input, int(float(thresh_rel/1000.0)*brightest_pixel), 255, cv2.THRESH_BINARY)
+		image_threshEnabled = img_thresh
+	else:			
+		thresh_abs = cv2.getTrackbarPos(trackbar_thresh_abs, title_window)
+		thresh_rel_check = int(float(thresh_abs/brightest_pixel)*1000)
+		cv2.setTrackbarPos(trackbar_thresh_rel, title_window, thresh_rel_check)
+		thresh_rel = thresh_rel_check
+		ret, img_thresh = cv2.threshold(image_input, thresh_abs, 255, cv2.THRESH_BINARY)
+		image_threshEnabled = img_thresh
+	return image_threshEnabled
+
+
+
 img=cv2.imread("./test_image.png")
 img_ablur = cv2.blur(img, (5, 5)) #---averaging
 img_gblur = cv2.GaussianBlur(img, (5, 5), 0) #---Gaussian blurring
@@ -21,94 +103,39 @@ img_gray = cv2.cvtColor(img_ablur, cv2.COLOR_BGR2GRAY)
 brightest_pixel = np.max(img_gray)
 darkest_pixel = np.min(img_gray)
 
-
-#---create one initial enclosing circle for avtive contour model(snakes)
-# snakes=[]
-# s = np.linspace(0, 2*np.pi, 400)
-# r = 100 + 105*np.sin(s)
-# c = 832 + 105*np.cos(s)
-# init = np.array([r, c]).T
-# snakes.append(active_contour(gaussian(img_thresh, 3, preserve_range=False), init, alpha=0.015, beta=10, gamma=0.001))
-
-
 #---create title window
 title_window = "ICG Image"
-trackbar_thresh = "Threshold"
-cv2.namedWindow(title_window, cv2.WINDOW_AUTOSIZE)
-
-
-
-def nothing(x):
-    pass
-#---create switch for ON/OFF functionality
 switch_color = 'Color OF/OFF'
 switch_thresh = 'Thresh ON/OFF'
-switch_display = 'Ralative ON/OFF'
+switch_relative = 'Relative ON/OFF'
 trackbar_thresh_abs = "Thresh_abs"
 trackbar_thresh_rel = "Thresh_rel(‰)"
-cv2.createTrackbar(switch_color, title_window, 0, 1, nothing)
-cv2.createTrackbar(switch_thresh, title_window, 0, 1, nothing)
-cv2.createTrackbar(switch_display, title_window, 0, 1, nothing)
 thresh_abs_slider_max = 254
 thresh_rel_slider_max = 1000
-cv2.createTrackbar(trackbar_thresh_abs, title_window, 90, thresh_abs_slider_max, nothing)
-cv2.createTrackbar(trackbar_thresh_rel, title_window, 354, thresh_rel_slider_max, nothing)
-img_shown = img_gray
+cv2.namedWindow(title_window, cv2.WINDOW_AUTOSIZE)
+createGUI()
+sc = 0
+st = 0
 
 while(1):
-#	img_shown = img_gray.copy()
+	img_shown = img.copy()
 	thresh_abs = cv2.getTrackbarPos(trackbar_thresh_abs, title_window)
 	ret, img_thresh = cv2.threshold(img_gray, thresh_abs, 255, cv2.THRESH_BINARY)
 	contours, hierarchy = cv2.findContours(img_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	# for c in cnts:
-	# 	M = cv2.moments(c)# CV2.moments會傳回一系列的moments值，我們只要知道中點X, Y的取得方式是如下進行即可。
-	# 	cX = int(M["m10"] / M["m00"])
-	# 	cY = int(M["m01"] / M["m00"])
-	# 	cv2.circle(img, (cX, cY), 5, (1, 227, 254), -1) # 在中心點畫上黃色實心圓
-	# 	((x, y), radius) = cv2.minEnclosingCircle(c)
-	# 	scaling_factor=1.2
-	# 	radius=radius*scaling_factor
-	# 	cv2.circle(img, (int(x), int(y)), int(radius), (0, 255, 0), 2) # 畫擬合的圓，半徑放大1.2倍
-	# 	s = np.linspace(0, 2*np.pi, 400)
-	# 	r = y + radius*np.sin(s)
-	# 	c = x + radius*np.cos(s)
-	# 	init = np.array([r, c]).T
-	#	snakes.append(active_contour(gaussian(img_thresh, 3, preserve_range=False), init, alpha=0.015, beta=10, gamma=0.001))
-	#	print(snakes)
-
-
-	# get current positions of four trackbars
-	sc = cv2.getTrackbarPos(switch_color, title_window)
-	st = cv2.getTrackbarPos(switch_thresh, title_window)
-	sd = cv2.getTrackbarPos(switch_display, title_window)
-#	print(sc, st, sd)
-
-
-	if sc == 0: #color is disabled
-#		print("st=", st)
-		if st == 0: #threshold is disabled
-			thresh_abs = cv2.getTrackbarPos(trackbar_thresh_abs, title_window)
-#			print("thresh_abs=",thresh_abs)
-			img_shown=img_gray.copy()
-			cv2.drawContours(img_shown, contours, -1, (255, 0, 0), 2) # 畫出findContours的結果
-		else:
-			if sd == 0:
-				thresh_abs = cv2.getTrackbarPos(trackbar_thresh_abs, title_window)
-				thresh_rel_check = int(float(thresh_abs/brightest_pixel)*1000)
-				cv2.setTrackbarPos(trackbar_thresh_rel, title_window, thresh_rel_check)
-				thresh_rel = thresh_rel_check
-				ret, img_thresh = cv2.threshold(img_gray-2, thresh_abs, 255, cv2.THRESH_BINARY)
-				img_shown = img_thresh
-			else:
-				thresh_rel = cv2.getTrackbarPos(trackbar_thresh_rel, title_window)
-				ret, img_thresh = cv2.threshold(img_gray-2, int(float(thresh_rel/1000.0)*brightest_pixel), 255, cv2.THRESH_BINARY)
-				img_shown = img_thresh
-	else: #---color is enabled
-		cv2.setTrackbarPos(switch_thresh, title_window, 0)
-		cv2.setTrackbarPos(switch_display, title_window, 0)
-		img_shown = cv2.applyColorMap(img_gray, cv2.COLORMAP_JET)
-		cv2.drawContours(img_shown, contours, -1, (255, 0, 0), 2) # 畫出findContours的結果
-
+	sc_last = sc
+	st_last = st
+	sc, st = updateSwitchValues(sc_last, st_last)
+	if st == 1: #---threshold ON
+		img_shown = applyThresh(img_gray) #---apply theshold binarization for img_gray
+	else:
+		if sc == 1: #---color ON, threshold OFF
+			img_shown = cv2.applyColorMap(img_gray, cv2.COLORMAP_JET) #---apply color map for img_gray
+			cv2.drawContours(img_shown, contours, -1, (0, 0, 0), 2)   #---plot contours on img_shown 
+		else:       #---color OFF, threshold OFF -> original image
+			cv2.drawContours(img_shown, contours, -1, (0, 255, 0), 2) #---plot contours on img_shown
+		
+		
+		
 	cv2.imshow(title_window, img_shown)
 #	cv2.waitKey(0)
 	k = cv2.waitKey(1) & 0xFF
